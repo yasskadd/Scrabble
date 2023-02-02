729d59d354aef7c5f6565d350e2e5228d66e41be
yasskadd <yasser.kadimi@gmail.com>
2023-02-02 04:58:36 -0500

    added socketIO server as parameter to broadcast joinedRoom event to everyone including the sender

--- server/app/services/client-utilities/home-chatbox-handler.service.ts
+++ server/app/services/client-utilities/home-chatbox-handler.service.ts
@@ -1,7 +1,7 @@
 import { GameRoom } from '@app/interfaces/game-room';
 import { SocketManager } from '@app/services/socket/socket-manager.service';
 import { SocketEvents } from '@common/constants/socket-events';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { Service } from 'typedi';
 import * as uuid from 'uuid';
 
@@ -18,8 +18,8 @@ export class HomeChatBoxHandlerService {
     }
 
     initSocketEvents() {
-        this.socketManager.on(SocketEvents.JoinHomeRoom, (socket, username: string) => {
-            this.joinHomeRoom(socket, username);
+        this.socketManager.io(SocketEvents.JoinHomeRoom, (sio: Server, socket: Socket, username: string) => {
+            this.joinHomeRoom(sio, socket, username);
         });
     }
 
@@ -33,12 +33,12 @@ export class HomeChatBoxHandlerService {
         } as HomeRoom;
     }
 
-    private joinHomeRoom(socket: Socket, username: string) {
+    private joinHomeRoom(sio: Server, socket: Socket, username: string) {
         if (this.homeRoom.isAvailable) {
             this.homeRoom.users.push(username);
             this.homeRoom.socketID.push(socket.id);
             socket.join(this.homeRoom.id);
-            this.userJoinedRoom(socket, username, this.homeRoom.id);
+            this.userJoinedRoom(sio, username, this.homeRoom.id);
             if (this.homeRoom.users.length === ROOM_LIMIT) this.homeRoom.isAvailable = false;
             console.log(`${username} joined the room: ${this.homeRoom.id}`);
         }
@@ -46,7 +46,7 @@ export class HomeChatBoxHandlerService {
     }
 
     // Method to notify clients that someone joined the room
-    private userJoinedRoom(socket: Socket, username: string, roomID: string) {
-        socket.to(roomID).emit(SocketEvents.JoinedHomeRoom, username);
+    private userJoinedRoom(sio: Server, username: string, roomID: string) {
+        sio.sockets.to(roomID).emit(SocketEvents.JoinedHomeRoom, username);
     }
 }
