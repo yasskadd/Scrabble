4e11ead98f47d9f71108ee1b77afc1a9b61e8f2e
yasskadd <yasser.kadimi@gmail.com>
2023-02-02 04:52:32 -0500

    initialization of chatboxhandler socketevents in the SocketSubscribeHandler

--- server/app/services/socket/socket-subscribe-handler.service.ts
+++ server/app/services/socket/socket-subscribe-handler.service.ts
@@ -1,5 +1,6 @@
 import { ChatboxHandlerService } from '@app/services/client-utilities/chatbox-handler.service';
 import { GameSessions } from '@app/services/client-utilities/game-sessions.service';
+import { HomeChatBoxHandlerService } from '@app/services/client-utilities/home-chatbox-handler.service';
 import { GamesActionsService } from '@app/services/games-management/games-actions.service';
 import { GamesStateService } from '@app/services/games-management/games-state.service';
 import { Service } from 'typedi';
@@ -8,6 +9,7 @@ import { Service } from 'typedi';
 export class SocketSubscribeHandler {
     constructor(
         private chatBoxHandlerService: ChatboxHandlerService,
+        private homeChatBoxHandler: HomeChatBoxHandlerService,
         private gameSessions: GameSessions,
         private gameActions: GamesActionsService,
         private gamesState: GamesStateService,
@@ -16,6 +18,7 @@ export class SocketSubscribeHandler {
     initSocketsEvents() {
         this.gameSessions.initSocketEvents();
         this.chatBoxHandlerService.initSocketsEvents();
+        this.homeChatBoxHandler.initSocketEvents();
         this.gameActions.initSocketsEvents();
         this.gamesState.initSocketsEvents();
     }
