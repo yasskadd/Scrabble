f3b5759ebf7bdb25c90fef2b36c492ac3666d203
yasskadd <yasser.kadimi@gmail.com>
2023-02-02 02:47:52 -0500

    implemented method to create empty room on server

--- server/app/services/client-utilities/home-chatbox-handler.service.ts
+++ server/app/services/client-utilities/home-chatbox-handler.service.ts
@@ -1,21 +1,27 @@
 import { GameRoom } from "@app/interfaces/game-room";
+import { Service } from 'typedi';
 import * as uuid from 'uuid';
 import { SocketManager } from "../socket/socket-manager.service";
 
-
+type MessageParameters = { roomId: string, message: string }
 type HomeRoom = Pick<GameRoom, 'id' | 'users' | 'socketID' | 'isAvailable'>;
+
+@Service()
 export class HomeChatBoxHandler {
-    private homeRoom:
-    private messageList:
-
-    constructor(public socketManager: SocketManager) {
-
-    }
+    private homeRoom: HomeRoom;
+    private messageMap = new Map<string, MessageParameters>()
+    constructor(public socketManager: SocketManager) {}
 
     private initSocketEvents()
 
     private initGameRoom() {
         const roomID = uuid.v4();
+        this.homeRoom = {
+            id: roomID,
+            users: [],
+            socketID: [],
+            isAvailable: true,
+        } as HomeRoom
     }
 
     private joinHomeRoom()
