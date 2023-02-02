77dc4db38ea0bfd5e2442f57bdba70afb30e9fad
yasskadd <yasser.kadimi@gmail.com>
2023-02-02 03:23:52 -0500

    started implementing socket to join room

--- server/app/services/client-utilities/home-chatbox-handler.service.ts
+++ server/app/services/client-utilities/home-chatbox-handler.service.ts
@@ -1,18 +1,27 @@
-import { GameRoom } from "@app/interfaces/game-room";
+import { GameRoom } from '@app/interfaces/game-room';
+import { SocketManager } from '@app/services/socket/socket-manager.service';
+import { SocketEvents } from '@common/constants/socket-events';
+import { Socket } from 'socket.io';
 import { Service } from 'typedi';
 import * as uuid from 'uuid';
-import { SocketManager } from "../socket/socket-manager.service";
 
-type MessageParameters = { roomId: string, message: string }
+type MessageParameters = { roomId: string; message: string };
 type HomeRoom = Pick<GameRoom, 'id' | 'users' | 'socketID' | 'isAvailable'>;
+const ROOM_LIMIT = 4;
 
 @Service()
 export class HomeChatBoxHandler {
     private homeRoom: HomeRoom;
-    private messageMap = new Map<string, MessageParameters>()
-    constructor(public socketManager: SocketManager) {}
+    private messageList = [];
+    constructor(public socketManager: SocketManager) {
+        this.initGameRoom();
+    }
 
-    private initSocketEvents()
+    private initSocketEvents() {
+        this.socketManager.on(SocketEvents.JoinHomeRoom, (socket, username: string) => {
+            this.joinHomeRoom(socket, username);
+        });
+    }
 
     private initGameRoom() {
         const roomID = uuid.v4();
@@ -21,8 +30,15 @@ export class HomeChatBoxHandler {
             users: [],
             socketID: [],
             isAvailable: true,
-        } as HomeRoom
+        } as HomeRoom;
     }
 
-    private joinHomeRoom()
+    private joinHomeRoom(socket: Socket, username: string) {
+        if (this.homeRoom.isAvailable) {
+            this.homeRoom.users.push(username);
+            this.homeRoom.socketID.push(socket);
+            if (this.homeRoom.users.length === ROOM_LIMIT) this.homeRoom.isAvailable = false;
+        }
+        // Implement sockets to give feedback of full room to client
+    }
 }
