8fc7d37d52b2e09f0fda9f1773db17629a852e4c
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2023-02-08 19:11:22 -0500

    fixed header to accomodate chat button in header

--- desktop/src/app/app.module.ts
+++ desktop/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -93,6 +94,7 @@ import { WaitingOpponentPageComponent } from './pages/waiting-opponent-page/wait
         MatProgressSpinnerModule,
         MatStepperModule,
         MatAutocompleteModule,
+        MatSidenavModule,
     ],
     providers: [ImportDictionaryComponent],
     bootstrap: [AppComponent],
--- desktop/src/app/components/header/header.component.ts
+++ desktop/src/app/components/header/header.component.ts
@@ -7,18 +7,27 @@ import { Router } from '@angular/router';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    buttonText: string[];
     readonly homePage: string[] = ['A', 'C', 'C', 'U', 'E', 'I', 'L'];
     readonly adminPage: string[] = ['A', 'D', 'M', 'I', 'N'];
-    constructor(private router: Router) {}
+    isHomePage: boolean;
+
+    constructor(private router: Router) {
+        this.isHomePage = this.checkIfHomePage();
+    }
+
+    checkIfHomePage() {
+        return this.router.url === `/home`;
+    }
 
     redirectHome() {
+        this.isHomePage = true;
         this.router.navigate(['/home']);
-        this.buttonText = this.adminPage;
+        console.log(this.isHomePage);
     }
 
     redirectAdmin() {
+        this.isHomePage = false;
         this.router.navigate(['/admin']);
-        this.buttonText = this.homePage;
+        console.log(this.isHomePage);
     }
 }
