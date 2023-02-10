9e5aafdeb3ad2de0274d82f552a3f47601993dd7
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2023-02-09 20:02:00 -0500

    main page interface fixed in order to accomodate sidenad toggle

---
+++ desktop/src/app/components/generic-chat/generic-chat.component.ts
@@ -0,0 +1,63 @@
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ChatboxMessage } from '@app/interfaces/chatbox-message';
+import { ChatboxHandlerService } from '@app/services/chatbox-handler.service';
+
+@Component({
+    selector: 'app-generic-chat',
+    templateUrl: './generic-chat.component.html',
+    styleUrls: ['./generic-chat.component.scss'],
+})
+export class GenericChatComponent implements AfterViewInit, AfterViewChecked {
+    static readonly inputInitialState = '';
+
+    @ViewChild('chatbox', { static: false }) chatbox: ElementRef;
+    @ViewChild('container') private scrollBox: ElementRef;
+
+    input = new FormControl(GenericChatComponent.inputInitialState);
+    private lastMessage: ChatboxMessage;
+
+    constructor(private chatboxHandler: ChatboxHandlerService) {}
+
+    get messages() {
+        return this.chatboxHandler.messages;
+    }
+
+    @HostListener('click')
+    clickInside() {
+        this.chatbox.nativeElement.focus();
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.chatboxHandler.resetMessage();
+            this.chatbox.nativeElement.focus();
+        }, 0);
+    }
+
+    submit() {
+        this.chatboxHandler.submitMessage(this.input.value);
+        this.resetInput();
+    }
+
+    submitMessage(message: string) {
+        this.input.setValue(message);
+        this.submit();
+    }
+
+    ngAfterViewChecked(): void {
+        const lastMessage = this.chatboxHandler.messages[this.chatboxHandler.messages.length - 1];
+        if (this.lastMessage !== lastMessage) {
+            this.lastMessage = lastMessage;
+            this.scrollToBottom();
+        }
+    }
+
+    private resetInput() {
+        this.input.setValue('');
+    }
+
+    private scrollToBottom(): void {
+        this.scrollBox.nativeElement.scrollTop = this.scrollBox.nativeElement.scrollHeight;
+    }
+}
