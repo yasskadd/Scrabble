4f7e780cce71f3761fe1cc815e8b9ff6908fb267
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-06 15:50:37 -0500

    refactored languages choice

--- desktop/src/app/pages/main-page/main-page.component.ts
+++ desktop/src/app/pages/main-page/main-page.component.ts
@@ -63,7 +63,6 @@ export class MainPageComponent implements OnDestroy {
         this.highScore.open(DialogBoxHighScoresComponent, {
             width: this.dialogWidthHighScore,
             panelClass: 'highScoreComponent',
-            disableClose: true,
         });
     }
 
