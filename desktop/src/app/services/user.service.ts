f49ede49229a866cef8a9ac73e59bf4d0147195f
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-13 17:07:34 -0400

    removed notes

--- desktop/src/app/services/user.service.ts
+++ desktop/src/app/services/user.service.ts
@@ -16,12 +16,6 @@ export class UserService {
         this.initUser();
     }
 
-    // TODO
-    // 1. Prendre un JWT token avec les infos du user
-    // 2. On store le token
-    // 3. On utilise le token comme cookie avec chaque requette de socket
-    // 4. Capturer la réponse si connexion invalide
-
     isConnected(): boolean {
         return this.user.username && this.user.password ? true : false;
     }
