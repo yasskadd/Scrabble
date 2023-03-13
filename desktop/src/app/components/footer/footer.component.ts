298ffcc87f24ed3c9b5f6f4db7ba26c027e2ecd0
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2023-03-13 16:52:29 -0400

    language service is in footer instead of header

--- desktop/src/app/components/footer/footer.component.ts
+++ desktop/src/app/components/footer/footer.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { LanguageChoice } from '@app/models/language-choice';
+import { LanguageService } from '@app/services/language.service';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+    protected languageForm: FormControl;
+    protected languageChoices: typeof LanguageChoice = LanguageChoice;
+
+    constructor(private languageService: LanguageService) {
+        this.languageForm = new FormControl(this.languageService.language, Validators.required);
+        this.languageForm.valueChanges.subscribe((value: string) => {
+            this.languageService.setLanguage(value as LanguageChoice);
+        });
+    }
+
+    getLetters(translation: string): string[] {
+        const letters: string[] = [];
+        this.languageService.getWord(translation).subscribe((word: string) => {
+            for (let i = 0; i < word.length; i++) {
+                letters.push(word.charAt(i));
+            }
+        });
+
+        return letters;
+    }
+}
--- desktop/src/app/components/header/header.component.ts
+++ desktop/src/app/components/header/header.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AppRoutes } from '@app/models/app-routes';
-import { LanguageChoice } from '@app/models/language-choice';
-import { LanguageService } from '@app/services/language.service';
 import { ThemeService } from '@services/theme.service';
 
 @Component({
@@ -14,16 +11,8 @@ import { ThemeService } from '@services/theme.service';
 export class HeaderComponent {
     isHomePage: boolean;
 
-    protected languageForm: FormControl;
-    protected languageChoices: typeof LanguageChoice = LanguageChoice;
-
-    constructor(private languageService: LanguageService, private router: Router, protected themeService: ThemeService) {
+    constructor(private router: Router, protected themeService: ThemeService) {
         this.isHomePage = this.checkIfHomePage();
-
-        this.languageForm = new FormControl(this.languageService.language, Validators.required);
-        this.languageForm.valueChanges.subscribe((value: string) => {
-            this.languageService.setLanguage(value as LanguageChoice);
-        });
     }
 
     checkIfHomePage() {
@@ -38,16 +27,5 @@ export class HeaderComponent {
     redirectAdmin() {
         this.isHomePage = false;
         this.router.navigate([AppRoutes.AdminPage]).then();
-    }
-
-    getLetters(translation: string): string[] {
-        const letters: string[] = [];
-        this.languageService.getWord(translation).subscribe((word: string) => {
-            for (let i = 0; i < word.length; i++) {
-                letters.push(word.charAt(i));
-            }
-        });
-
-        return letters;
     }
 }
