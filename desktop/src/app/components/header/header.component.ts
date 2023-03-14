5b4add3ae3093c51c8ba5eb4d3749dc40060f979
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2023-03-14 14:42:36 -0400

    bug fix

--- desktop/src/app/components/header/header.component.ts
+++ desktop/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppRoutes } from '@app/models/app-routes';
 import { LanguageService } from '@app/services/language.service';
+import { ThemeService } from '@app/services/theme.service';
 import { UserService } from '@app/services/user.service';
 
 @Component({
@@ -11,7 +12,12 @@ import { UserService } from '@app/services/user.service';
 })
 export class HeaderComponent {
     isHomePage: boolean;
-    constructor(protected userService: UserService, private router: Router, private languageService: LanguageService) {
+    constructor(
+        protected userService: UserService,
+        private router: Router,
+        private languageService: LanguageService,
+        protected themeService: ThemeService,
+    ) {
         this.isHomePage = this.checkIfHomePage();
     }
 
