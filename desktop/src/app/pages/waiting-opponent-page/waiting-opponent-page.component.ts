8955d11e68fe8b90d52aade21c7ff7d1fb28172f
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-18 21:28:01 -0400

    applied the last changes in the components

--- desktop/src/app/pages/waiting-opponent-page/waiting-opponent-page.component.ts
+++ desktop/src/app/pages/waiting-opponent-page/waiting-opponent-page.component.ts
@@ -1,46 +1,33 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppRoutes } from '@app/models/app-routes';
 import { GameConfigurationService } from '@app/services/game-configuration.service';
 import { UserService } from '@app/services/user.service';
-import { SnackBarService } from '@services/snack-bar.service';
 
 @Component({
     selector: 'app-waiting-opponent-page',
     templateUrl: './waiting-opponent-page.component.html',
     styleUrls: ['./waiting-opponent-page.component.scss'],
 })
-export class WaitingOpponentPageComponent implements OnInit, OnDestroy {
-    gameMode: string;
-
+export class WaitingOpponentPageComponent implements OnDestroy, AfterContentChecked {
     constructor(
         protected gameConfiguration: GameConfigurationService,
         protected userService: UserService,
         private router: Router,
-        private snackBarService: SnackBarService,
         private activatedRoute: ActivatedRoute,
     ) {
-        this.gameMode = this.activatedRoute.snapshot.params.id;
+        this.gameConfiguration.gameMode = this.activatedRoute.snapshot.params.id;
     }
 
-    ngOnInit(): void {
+    ngAfterContentChecked(): void {
         this.listenToServerResponse();
     }
 
     ngOnDestroy() {
-        this.gameConfiguration.errorReason.unsubscribe();
         this.gameConfiguration.isRoomJoinable.unsubscribe();
     }
 
     listenToServerResponse() {
-        this.gameConfiguration.errorReason.subscribe((error: string) => {
-            if (error !== '') {
-                this.snackBarService.openError(error);
-                this.exitRoom();
-            }
-            this.exitRoom(false);
-        });
-
         this.gameConfiguration.isGameStarted.subscribe((value) => {
             if (value) {
                 this.joinGamePage();
@@ -50,21 +37,10 @@ export class WaitingOpponentPageComponent implements OnInit, OnDestroy {
 
     joinSoloMode() {
         this.gameConfiguration.removeRoom();
-        this.router.navigate([`${AppRoutes.SoloGameCreationPage}/${this.gameMode}`]).then();
+        this.router.navigate([`${AppRoutes.SoloGameCreationPage}/${this.gameConfiguration.gameMode}`]).then();
     }
 
     joinGamePage() {
         this.router.navigate([AppRoutes.GamePage]).then();
-    }
-
-    exitRoom(exitByIsOwn?: boolean) {
-        if (this.gameConfiguration.roomInformation.isCreator) {
-            this.router.navigate([`${AppRoutes.MultiGameCreationPage}/${this.gameMode}`]).then();
-            this.gameConfiguration.removeRoom();
-        } else {
-            this.router.navigate([`${AppRoutes.MultiJoinPage}/${this.gameMode}`]).then();
-            if (!exitByIsOwn) return;
-            this.gameConfiguration.exitWaitingRoom();
-        }
     }
 }
