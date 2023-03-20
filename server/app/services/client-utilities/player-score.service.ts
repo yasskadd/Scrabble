ad4d8d0179df62cdc35c85db2d5fea79d6c425ae
laurent.nguyen <laurent.nguyen@sentiom.com>
2023-03-20 19:39:17 -0400

    start of score service

---
+++ server/app/services/client-utilities/player-score.service.ts
@@ -0,0 +1,18 @@
+import { Service } from 'typedi';
+const LOSS_SCORE = -5;
+const WIN_SCORE = 10;
+
+@Service()
+export class PlayerScoreService {
+    constructor() {}
+
+    calculateScore(currentPlayerScore: number): number {}
+
+    lossScore(currentPlayerScore: number): number {
+        return currentPlayerScore + LOSS_SCORE;
+    }
+
+    winScore(currentPlayerScore: number): number {
+        return currentPlayerScore + WIN_SCORE;
+    }
+}
