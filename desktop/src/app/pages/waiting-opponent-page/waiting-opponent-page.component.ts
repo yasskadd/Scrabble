56b0216300a50af14663704fa8b31357d19af62a
michaelroussel404@outlook.com <michaelroussel404@outlook.com>
2023-03-24 23:18:08 -0400

    adapted the waiting room to the new architecture

--- desktop/src/app/pages/waiting-opponent-page/waiting-opponent-page.component.ts
+++ desktop/src/app/pages/waiting-opponent-page/waiting-opponent-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AppRoutes } from '@app/models/app-routes';
 import { GameConfigurationService } from '@app/services/game-configuration.service';
 import { UserService } from '@app/services/user.service';
+import { GameVisibility } from '@common/models/game-visibility';
 
 @Component({
     selector: 'app-waiting-opponent-page',
@@ -10,19 +11,23 @@ import { UserService } from '@app/services/user.service';
     styleUrls: ['./waiting-opponent-page.component.scss'],
 })
 export class WaitingOpponentPageComponent implements OnDestroy, AfterContentChecked {
+    protected gameVisibility: typeof GameVisibility = GameVisibility;
+
+    private gameMode: string;
+
     constructor(
         protected gameConfiguration: GameConfigurationService,
         protected userService: UserService,
         private router: Router,
         private activatedRoute: ActivatedRoute,
     ) {
-        this.gameConfiguration.gameMode = this.activatedRoute.snapshot.params.id;
+        this.gameMode = this.activatedRoute.snapshot.params.id;
     }
 
     // TODO : Check for page refresh also
     @HostListener('window:popstate', ['$event'])
     onPopState() {
-        this.gameConfiguration.exitRoom();
+        // this.gameConfiguration.exitWaitingRoom();
     }
 
     ngAfterContentChecked(): void {
@@ -30,6 +35,7 @@ export class WaitingOpponentPageComponent implements OnDestroy, AfterContentChec
     }
 
     ngOnDestroy() {
+        this.gameConfiguration.exitWaitingRoom();
         this.gameConfiguration.isRoomJoinable.unsubscribe();
     }
 
@@ -48,5 +54,13 @@ export class WaitingOpponentPageComponent implements OnDestroy, AfterContentChec
 
     joinGamePage() {
         this.router.navigate([AppRoutes.GamePage]).then();
+    }
+
+    exitWaitingRoom(): void {
+        if (this.gameConfiguration.isGameCreator()) {
+            this.router.navigate([`${AppRoutes.MultiGameCreationPage}/${this.gameMode}`]).then();
+        } else {
+            this.router.navigate([`${AppRoutes.MultiJoinPage}/${this.gameMode}`]).then();
+        }
     }
 }
