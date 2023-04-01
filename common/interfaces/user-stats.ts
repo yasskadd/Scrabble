9cd0b4c67207df8549ee2b9f224180d44e8e2d09
laurent.nguyen <laurent.nguyen@sentiom.com>
2023-04-01 03:34:25 -0400

    added user stats interface

---
+++ common/interfaces/user-stats.ts
@@ -0,0 +1,9 @@
+export interface UserStats {
+    _id: string;
+    score: number;
+    gamePlayed: number;
+    win: number;
+    loss: number;
+    totalDuration: number;
+    gameScore: number;
+}
