53b7c4084dc7c11c53155a31336cf4ae82fc86ea
laurent.nguyen <laurent.nguyen@sentiom.com>
2023-04-02 03:17:25 -0400

    updated user-stats service

--- server/app/services/database/user-stats-storage.service.ts
+++ server/app/services/database/user-stats-storage.service.ts
@@ -6,73 +6,60 @@ import { Document, ObjectId } from 'mongodb';
 import { Service } from 'typedi';
 import { DatabaseService } from './database.service';
 
+const SECOND_IN_MILLISECOND = 1000;
+const SECOND_AND_MINUTE_MAX_VALUE = 60;
+const MINIMUM_TWO_UNITS = 10;
 @Service()
 export class UserStatsStorageService {
     constructor(private database: DatabaseService) {}
 
     async getUserStats(id: string): Promise<UserStats> {
-        const userStats = (await this.database.usersStats.fetchDocuments({ _id: new ObjectId(id) })) as Document;
+        const userStats = (await this.database.usersStats.fetchDocuments({ userIdRef: new ObjectId(id) }))[0] as Document;
         return userStats as UserStats;
     }
 
     async updatePlayerStats(gameHistoryInfo: GameHistoryInfo): Promise<void> {
-        await this.updateScore(gameHistoryInfo);
-        await this.updateGameStats(gameHistoryInfo);
-    }
-
-    private async updateScore(gameHistoryInfo: GameHistoryInfo): Promise<void> {
-        if (gameHistoryInfo.playerWonGame) {
-            await this.database.usersStats.collection.updateOne(
-                { _id: new ObjectId(gameHistoryInfo.playerId) },
-                {
-                    $inc: {
-                        score: {
-                            $cond: {
-                                if: { $lt: ['$score', MAX_SCORE - WIN_SCORE] },
-                                then: WIN_SCORE,
-                                else: MAX_SCORE,
-                            },
-                        },
-                    },
-                },
-            );
-        } else {
-            await this.database.usersStats.collection.updateOne(
-                { _id: new ObjectId(gameHistoryInfo.playerId) },
-                {
-                    $inc: {
-                        score: {
-                            $cond: {
-                                if: { $gt: ['$score', MIN_SCORE - LOSS_SCORE] },
-                                then: LOSS_SCORE,
-                                else: MIN_SCORE,
-                            },
-                        },
-                    },
-                },
-            );
-        }
-    }
-
-    private async updateGameStats(gameHistoryInfo: GameHistoryInfo): Promise<void> {
-        await this.database.users.collection.updateOne(
-            { _id: new ObjectId(gameHistoryInfo.playerId) },
+        const userStats = await this.getUserStats(gameHistoryInfo.playerId);
+        const newScore = this.calculateScore(userStats.score, gameHistoryInfo.playerWonGame);
+        const newGamePlayed = userStats.gamePlayed + 1;
+        const newWin = gameHistoryInfo.playerWonGame ? userStats.win + 1 : userStats.win;
+        const newLoss = gameHistoryInfo.playerWonGame ? userStats.loss : userStats.loss + 1;
+        const newTotalGameTime = userStats.totalGameTime + gameHistoryInfo.duration;
+        const newTotalGameScore = userStats.totalGameScore + gameHistoryInfo.playerScore;
+        const newAverageGameTime = this.msToMinSec(newTotalGameTime / newGamePlayed);
+        const newAverageGameScore = newTotalGameScore / newGamePlayed;
+        await this.database.usersStats.collection.updateOne(
+            { userIdRef: new ObjectId(gameHistoryInfo.playerId) },
             {
-                $inc: {
-                    $cond: {
-                        if: { $eq: [gameHistoryInfo.playerWonGame, true] },
-                        then: { win: 1 },
-                        else: { loss: 1 },
-                    },
-                    gamePlayed: 1,
-                    totalDuration: gameHistoryInfo.duration,
-                    totalGamePoints: gameHistoryInfo.playerScore,
-                },
                 $set: {
-                    averageGameTime: { $divide: ['$totalDuration', '$gamePlayed'] },
-                    averageGamePoints: { $divide: ['$totalGamePoints', '$gamePlayed'] },
+                    score: newScore,
+                    gamePlayed: newGamePlayed,
+                    win: newWin,
+                    loss: newLoss,
+                    totalGameTime: newTotalGameTime,
+                    totalGameScore: newTotalGameScore,
+                    averageGameTime: newAverageGameTime,
+                    averageGameScore: newAverageGameScore,
                 },
             },
         );
+    }
+
+    private calculateScore(originalScore: number, playerWonGame: boolean): number {
+        let newScore: number;
+        if (playerWonGame) {
+            newScore = originalScore + WIN_SCORE;
+            return newScore < MAX_SCORE ? newScore : MAX_SCORE;
+        }
+
+        newScore = originalScore + LOSS_SCORE;
+        return newScore > MIN_SCORE ? newScore : MIN_SCORE;
+    }
+
+    private msToMinSec(milliseconds: number): string {
+        const seconds = Math.floor((milliseconds / SECOND_IN_MILLISECOND) % SECOND_AND_MINUTE_MAX_VALUE);
+        const minutes = Math.floor(milliseconds / (SECOND_IN_MILLISECOND * SECOND_AND_MINUTE_MAX_VALUE));
+
+        return (minutes < MINIMUM_TWO_UNITS ? '0' + minutes : minutes) + ':' + (seconds < MINIMUM_TWO_UNITS ? '0' + seconds : seconds);
     }
 }
