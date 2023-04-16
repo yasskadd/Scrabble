6a1acd02eb07be8f5443a67280dcec368cc2c612
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2023-04-16 17:48:28 -0400

    language

--- desktop/src/app/components/dialog-box-reconnection/dialog-box-reconnection.component.ts
+++ desktop/src/app/components/dialog-box-reconnection/dialog-box-reconnection.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '@app/services/language.service';
 
 @Component({
     selector: 'app-dialog-box-reconnection',
     templateUrl: './dialog-box-reconnection.component.html',
     styleUrls: ['./dialog-box-reconnection.component.scss'],
 })
-export class DialogBoxReconnectionComponent {}
+export class DialogBoxReconnectionComponent {
+    constructor(protected languageService: LanguageService) {}
+}
